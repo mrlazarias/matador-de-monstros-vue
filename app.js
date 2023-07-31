855cbe644d6cf7885e5dca4eab298826d11e3d82
Murilo <muriloazarias97@gmail.com>
2023-07-31 08:36:40 -0300

    Finished

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
         running: false,
         playerLife: 100,
         monsterLife: 100,
+        logs: []
     },
     computed: {
         hasResult() {
@@ -14,28 +15,36 @@ new Vue({
         startGame() {
             this.running = true
             this.playerLife = 100
-            this.monsterLife= 100
+            this.monsterLife = 100
+            this.logs = []
         },
         attack(especial) {
-            this.hurt('monsterLife', 5, 10, true)
-            this.hurt('playerLife', 7, 12, false)
+            this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player')
+            if(this.monsterLife > 0) {
+                this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
+            }
         },
-        hurt(prop, min, max, especial) {
+        hurt(prop, min, max, especial, source, target, cls) {
             const plus = especial ? 5 : 0
             const hurt = this.getRandom(min + plus, max + plus)
-            this[prop] = Math.max(this.playerLife - hurt, 0)
+            this[prop] = Math.max(this[prop] - hurt, 0)
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls)
         },
         healAndHurt() {
-            this.heal (10, 15)
-            this.hurt('playerLife', 7, 12, false)
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster')
         },
         heal(min, max) {
             const heal = this.getRandom(min, max)
             this.playerLife = Math.min(this.playerLife + heal, 100)
+            this.registerLog(`Jogador ganhou força de ${heal}.`, 'player')
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min
             return Math.round(value)
+        },
+        registerLog(text, cls) {
+            this.logs.unshift({ text, cls })
         }
     },
     watch: {
@@ -43,5 +52,4 @@ new Vue({
             if (value) this.running = false
         }
     }
-   
 })
